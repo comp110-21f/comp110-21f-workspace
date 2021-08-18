52ba2e13c88550a4bd7f2a5023f0cdafe89776da
Kris Jordan <kris@cs.unc.edu>
2021-08-18 15:37:01 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,51 @@
+# COMP110 Workspace - Summer Session I 2021
+
+Your work in the course will be completed in this workspace. Instructions for the two most common tasks you'll perform can be found below.
+
+## Backup Your Work
+
+"Push" your work up to GitHub for backup. By creating "commits", which you can think of as versioned checkpoints in your workspace, you are not at risk of losing your work. It's easy to revert back to an old version or to restore your entire workspace on a different computer.
+
+1. Select the _View_ menu and then _SCM_ (Source Control Management)
+   - Alternatively: Select the Activity Bar icon that's three circles with lines for Source Control
+2. Notice the files listed under Changes. These are files you've made modifications to since your last backup.
+3. Move your mouse's cursor over the word changes and notice the + symbol that appears. Click that plus symbol to add all changes to the next backup. You will now see these listed under "Staged Changes".
+   - If you do not want to backup _all_ changed files, you can select them individually. For this course you're encouraged to back everything up.
+4. In the Message box, give a brief description of what you've changed and are backing up. This will help you find a specific backup (called a "commit") if needed.
+5. Open the _View_ menu and select _Command Palette_, the shortcut for this menu is:
+   - Windows: `Control+Shift+P`
+   - Mac: `Command+Shift+P`
+6. Begin typing in: `Git: Push to...` and press `Enter` once it is the first option.
+7. Select the `backup` remote that is your personal workspace on GitHub. If you do not see `backup` listed, see the instructions below on _Setup Backup Course Material Repository_.
+   - You may see a spinning "refresh" icon in your status bar at the bottom of VSCode. Unless an error backing up occurs, you will not see any confirmation.
+   - If you want to see your backed up work on Github, navigate to the following URL but replace `USERNAME` with your GitHub username:
+   - `https://github.com/comp110-21f/comp110-workspace-21f-USERNAME`
+
+## Download New Course Material
+
+"Pulling" course materials down from Upstream. As new lesson material or starter code for exercises and projects is added to our central course repository, these are the steps you'll take to download them into your own workspace repository.
+
+1. Open the _View_ menu and select _Command Palette_ 
+   - The shortcut for this menu is:
+   - Windows: `Control+Shift+P`
+   - Mac: `Command+Shift+P`
+2. Begin typing in `Git: Pull From...` and press `Enter` once it is the first option. 
+3. Begin typing in `origin` and press `Enter` once it is the first option.
+4. Press enter with `origin/master` as the first option.
+5. This downloads the latest course materials! It will succeed silently, so if nothing appears to happen it worked (and any new files will be available in the file explorer). If there was an error, you would see an error message pop up.
+
+## Setup Backup Course Material Repository
+
+When you are first setting up your personal workspace repository, you will need to follow these instructions to connect the repository on your computer with your personal "backup" course repository on GitHub. As we add new material through the course, you will then be able to easily back it up to your backup repo.
+
+1. Navigate a web browser to <https://classroom.github.com/a/zgjDwIVk>
+2. Join the Classroom by selecting your ONYEN (Clicking "Skip to the next step" is also OK)
+3. Click "Accept this Assignment"
+4. When the workspace is getting setup you may need to refresh until it tells you you're ready to go!
+5. Click the link to your personal backup repository that looks something like this `https://github.com/comp110-21f/comp110-workspace-21f-krisjordan` (except instead of `krisjordan` you will see your GitHub username).
+6. Toward the top of the page it will say "Quick setup" and you will see two buttons: HTTPS and SSH. Click HTTPS and copy the text of the URL in the box to the right of the buttons (or click the clipboard icon)
+7. With your workspace open in VSCode, type in _Git: Add Remote_ and press enter with the option selected.
+10. In the blank text box that appears, paste in the URL to your backup repository that you just copied. Press enter.
+11. When asked for "Remote name" type in: `backup`
+12. Follow the steps in the section above on how to "Backup your work."
+
---
+++ tools/diagnostic.py
@@ -0,0 +1,266 @@
+"""Python module to diagnose possible missed installations or plugins on students computers..
+Usage:
+    python -m tools.diagnostic
+"""
+
+import colorama
+import sys
+import subprocess
+import shutil
+
+
+__author__ = "Ezri White <ezri@live.unc.edu>"
+
+
+colorama.init()  # Color for windows!
+
+
+class Check_Result:
+    """Contains information on status of a run test."""
+
+    def __init__(self, test_name):
+        """Set's up properties of a test result object."""
+        self.test_name = test_name
+        self.description = ""
+        self.status = False
+        self.fail_messages = []
+        self.success_messages = []
+        self.error_codes = []
+
+
+class bcolors:
+    """Colors for printing."""
+    HEADER = '\033[96m'
+    ITALIC = '\033[3m'
+    PASS = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = "\033[91m"
+    ECODE = '\033[95m'
+    END = '\033[0m'
+    BOLD = '\033[1m'
+    BLINK = '\033[5m'
+    UNDERLINE = '\033[4m'
+
+
+def main() -> None:
+    """Entry point of script. Expects to be run as CLI program."""
+    mod: str = verbose()
+    test_results = [test_python_in_path(), test_version(), test_pytest(), test_requirements(), test_git_in_PATH()]
+    if test_results[4].status:
+        test_results.append(test_git_config())
+        test_results.append(test_git_backup())
+    else:
+        test_results.append(test_not_run("Git Configuration Test",
+                                         "Check for the existence of git author and email configuration.",
+                                         "Git in Path test failed so test not run."))
+        test_results.append(test_not_run("Git Backup Test",
+                                         "Check for git backup repo set to the correct place.",
+                                         "Git in Path test failed so test not run."))
+    print("\n")
+    all_passed = True
+    for test in test_results:
+        if not test.status or (mod == "--verbose"):
+            print(wrap(bcolors.HEADER, "========== " + test.test_name + " =========="))
+            print(wrap(bcolors.ITALIC, test.description))
+            print(wrap(bcolors.UNDERLINE, "Status") + ": " + ((wrap(bcolors.PASS, "Passed"))
+                                                              if test.status else (wrap(bcolors.FAIL, "Failed"))))
+            for message in test.success_messages:
+                print(message)
+            for message in test.fail_messages:
+                print(wrap(bcolors.WARNING, message))
+            print("\n")
+            all_passed = False
+    if all_passed:
+        print(wrap(bcolors.PASS, "========== All Tests Passed ==========\n"))
+    else:
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        print(bcolors.ECODE + "The following diagnostic codes were raised:\n")
+        for test in test_results:
+            for code in test.error_codes:
+                print(code)
+        print("\nFor information on how to fix: https://21s.comp110.com/diagnostics.html" + bcolors.END)
+        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+
+def wrap(code, text) -> str:
+    """Wraps input string with format literal."""
+    return code + text + bcolors.END
+
+
+def test_python_in_path() -> Check_Result:
+    """Test for python versions in path and what python vs python3 commands output."""
+    python_in_path_test = Check_Result("Python in Path test")
+    python_in_path_test.description = "Checks what versions and binaries of python are present in PATH."
+    python_paths = search_path(python="python", python3="python3")
+    python_test, python3_test = python_paths[0], python_paths[1]
+    if python_test is not None: 
+        python_in_path_test.success_messages.append("Python Path: " + python_test)
+    if python3_test is not None:
+        python_in_path_test.success_messages.append("Python3 Path: " + python3_test)
+    try:
+        python_version = str(subprocess.check_output(["python", "--version"], stderr=subprocess.STDOUT))
+        if "win" not in sys.platform:
+            python_in_path_test.success_messages.append("Python Version: " + python_version[2:-3])
+        else:
+            python_in_path_test.success_messages.append("Python Version: " + python_version[2:-5])
+        if ("Python 3.9" in python_version):
+            python_in_path_test.status = True 
+        else:
+            python_in_path_test.fail_messages.append("Python interpreter is lower than 3.9.")
+            python_in_path_test.error_codes.append("S102 - python command runs a version less than 3.9 ")
+    except Exception:
+        python_version = ""
+        python_in_path_test.fail_messages.append("Python not installed.")
+        python_in_path_test.error_codes.append("S101 - Python is not installed")
+    return python_in_path_test
+
+
+def test_version() -> Check_Result:
+    """Test version of interpreter being run from vscode."""
+    version_test = Check_Result("Python Interpreter Test")
+    version_test.description = "Python version of interpreter that is being run in the workspace."
+    version = sys.version[:3]
+    if float(version) < 3.9:
+        version_test.fail_messages.append("Python interpreter: " + version)
+        version_test.fail_messages.append("Python interpreter used in workspace is lower than 3.9.")
+        version_test.error_codes.append("S103 - Python version being run in vscode is lower than 3.9")
+    else:
+        version_test.success_messages.append("Python interpreter: " + version)
+        version_test.status = True 
+
+    return version_test
+
+
+def test_requirements() -> Check_Result:
+    """Test for packages intalled from requirements.txt."""
+    requirements_test = Check_Result("Installed Packages Test")
+    requirements_test.description = "Check for all packages in requirements.txt installed with pip"
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0].lower() for r in reqs.split()]
+
+    with open('requirements.txt', 'r') as file:
+        lines = file.readlines()
+    required_packages = []
+    for line in lines:
+        if line[0].isalpha():
+            required_packages.append(line.split('\n')[0].lower())
+
+    missing_packages = True
+    for package in required_packages:
+        if package not in installed_packages:
+            requirements_test.fail_messages.append("Missing: " + package)
+            requirements_test.error_codes.append("S301 - Missing Required Packages")
+            missing_packages = False
+        else:
+            requirements_test.success_messages.append("Installed: " + package)
+    requirements_test.status = missing_packages
+
+    return requirements_test
+
+
+def test_git_in_PATH() -> Check_Result:
+    """Test for the existence of git in the PATH."""
+    git_path_test = Check_Result("Git on Path Test")
+    git_path_test.description = "Check for the existence of git in the PATH."
+    try:
+        git_version = subprocess.check_output(["git", "--version"], stderr=subprocess.STDOUT)
+        git_path_test.success_messages.append(str(git_version)[2:20].capitalize())
+        git_path_test.status = True
+    except Exception:
+        git_path_test.fail_messages.append("Git could not be found.")
+        git_path_test.error_codes.append("S201 - Git is not installed or not on path")
+        git_path_test.status = False
+    return git_path_test
+
+
+def test_git_config() -> Check_Result:
+    """Test for the existence of git author and email configuration."""
+    git_config_test = Check_Result("Git Configuration Test")
+    git_config_test.description = "Check for the existence of git author and email configuration."
+    try:
+        user_name = subprocess.check_output(["git", "config", "user.name"], stderr=subprocess.STDOUT)
+        email = subprocess.check_output(["git", "config", "user.email"], stderr=subprocess.STDOUT)
+        git_config_test.success_messages.append("User Name: " + str(user_name)[2:-3])
+        git_config_test.success_messages.append("Email: " + str(email)[2:-3])
+        git_config_test.status = True if '@' in str(email) else False
+    except Exception:
+        git_config_test.fail_messages.append("User Name: User name was not found.")
+        git_config_test.fail_messages.append("Email: Email was not found")
+        git_config_test.error_codes.append("S202 - Email and Username not configured")
+
+    return git_config_test
+
+
+def test_git_backup() -> Check_Result:
+    """Test for git backup repo set to the correct place."""
+    git_backup_test = Check_Result("Git backup Test")
+    git_backup_test.description = "Check for git backup repo set to the correct place."
+    try:
+        backup = str(subprocess.check_output(["git", "remote", "get-url", "backup"], stderr=subprocess.STDOUT))
+        git_backup_test.success_messages.append("backup: " + backup[2:-3])
+        if 'https://github.com/comp110-21s/comp110-workspace-21s-' in backup:
+            git_backup_test.status = True
+        else:
+            git_backup_test.status = False
+            git_backup_test.fail_messages.append("backup did not match course backup")
+            git_backup_test.error_codes.append("S204 - Incorrect backup found")
+
+    except Exception:
+        backup = "backup not found."
+        git_backup_test.fail_messages.append("backup: " + backup)
+        git_backup_test.error_codes.append("S203 - No backup found")
+
+    git_backup_test.status = True if 'https://github.com/comp110-21s/comp110-workspace-21s-' in backup else False
+    return git_backup_test
+
+
+def test_pytest() -> Check_Result:
+    """Test for errors finding pytests."""
+    pytest_test = Check_Result("Pytest Test")
+    pytest_test.description = "Test for errors finding pytests."
+    try:
+        collect_messages = str(subprocess.check_output(
+            ["python", "-m", "pytest", "--collect-only", "."], stderr=subprocess.STDOUT))
+        if 'error' not in collect_messages:
+            pytest_test.status = True
+    except Exception:
+        pytest_test.status = False
+        pytest_test.fail_messages.append(
+            "Test collection threw an exception, most likely syntax errors in workspace.")
+    return pytest_test
+
+
+def test_not_run(test_name, description, message) -> Check_Result:
+    """Create test result object for test not run."""
+    test = Check_Result(test_name)
+    test.description = description
+    test.fail_messages.append(message)
+    return test
+
+
+def search_path(**kwargs) -> list:
+    """Searches PATH for given executable strings and returns the list of executable paths; returns None if no path found."""
+    matches = []
+    for key, val in kwargs.items():
+        match = shutil.which(val)
+        matches.append(match)
+    return matches
+    
+
+def verbose() -> str:
+    """Check for verbose mode."""
+    if len(sys.argv) > 2:
+        print("Usage: python -m tools.diagnostic [--verbose]")
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == "--verbose":
+            return "--verbose"
+        else:
+            print("Usage: python -m tools.diagnostic [--verbose]")
+            sys.exit(1)
+    else:
+        return "quiet"
+
+
+if __name__ == "__main__":
+    main()
