d6dccc432072133d912b3405e4733960d0dff166
Anthony Edwards <antedw03@ad.unc.edu>
2021-08-31 21:25:44 -0400

    4th Edit to Practice Exercise 1

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -4,6 +4,8 @@ __author__ = "730518679"
 
 x = 11
 y = 6
+print("Left-hand side:", x)
+print("Right-hand side:", y)
 print("11 ** 6 is", x ** y)
 print("11 / 6 is", x / y)
 print("11 // 6 is", x // y)
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,9 +1,11 @@
-"This program sets two variables equal to a number and then compares them using various relational operators"
+"""This program sets two variables equal to a number and then compares them using various relational operators."""
 
 __author__ = "730518679"
 
 x = 11
 y = 6
+print("Left-hand side:", x)
+print("Right-hand side:", y)
 print("11 < 6 is", x < y)
 print("11 >= 6 is", x >= y)
 print("11 == 6 is", x == y)
