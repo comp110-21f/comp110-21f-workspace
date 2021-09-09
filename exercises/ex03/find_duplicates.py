10bc7fc432508d709325bd829749ad5246448fb1
Kaki Ryan <kakiryan6@gmail.com>
2021-09-09 12:23:59 -0400

    add ex03 starter code

---
+++ exercises/ex03/find_duplicates.py
@@ -0,0 +1,3 @@
+"""Finding duplicate letters in a word."""
+
+__author__ = "123456789"
---
+++ exercises/ex03/happy_trees.py
@@ -0,0 +1,6 @@
+"""Drawing forests in a loop."""
+
+__author__ = "123456789"
+
+# The string constant for the pine tree emoji
+TREE: str = '\U0001F332'
