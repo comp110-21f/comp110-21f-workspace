89285f852f170230ab1ca1fced16b2c27aabc393
Kaki Ryan <kakiryan6@gmail.com>
2021-09-30 00:08:35 -0400

    EX05

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,5 @@
+"""List utility functions part 2."""
+
+__author__ = "123456789"
+
+# Define your functions below
---
+++ exercises/ex05/utils_test.py
@@ -0,0 +1,6 @@
+"""Unit tests for list utility functions."""
+
+# TODO: Uncomment the below line when ready to write unit tests
+# from exercises.ex05.utils import only_evens, sub, concat
+
+__author__ = "123456789"
