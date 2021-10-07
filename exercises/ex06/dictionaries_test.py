52b07cf1693350358a90cc0b7e710798674a7d37
Kaki Ryan <kakiryan6@gmail.com>
2021-10-07 09:58:04 -0400

    Add ex06

---
+++ exercises/ex06/dictionaries_test.py
@@ -0,0 +1,6 @@
+"""Unit tests for dictionary functions."""
+
+# TODO: Uncomment the below line when ready to write unit tests
+# from exercises.ex06.dictionaries import invert, favorite_color, count
+
+__author__ = "123456789"
